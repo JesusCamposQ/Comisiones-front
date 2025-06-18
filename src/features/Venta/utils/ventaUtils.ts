ff846860a77ca3be7199b0e2cb54822a9a94631e
Juan carlos chocllu <j.chocllu@opti-system.com>
2025-06-18 12:25:31 -0400

    cambios

--- src/features/Venta/utils/ventaUtils.ts
+++ src/features/Venta/utils/ventaUtils.ts
@@ -1,29 +1,36 @@
-import { Comision, MetaProductosVip, VentaElement } from "../interfaces/venta.interface";
+import {
+  Comision,
+  MetaProductosVip,
+  VentaElement,
+} from "../interfaces/venta.interface";
 
-export function porcentaje(total:number, monto:number) {
-  if(total <= 0){
-    return 0
+export function porcentaje(total: number, monto: number) {
+  if (total <= 0) {
+    return 0;
   }
-    /*if(sucursal && sucursal.includes("PARAGUAY")){
+  /*if(sucursal && sucursal.includes("PARAGUAY")){
       return 3.00
     }*/
-    const porcentaje =(monto /total) * 100
-    return Number(porcentaje.toFixed(2)) || 0
+  const porcentaje = (monto / total) * 100;
+  return Number(porcentaje.toFixed(2)) || 0;
 }
 
-export function descontarPorcentajeAcomision(comision: number, porcentaje: number): number {
-    if (
-        !Number.isFinite(comision) || 
-        !Number.isFinite(porcentaje) || 
-        comision < 0 || 
-        porcentaje < 0 || 
-        porcentaje > 100
-    ) {
-        return 0;
-    }
+export function descontarPorcentajeAcomision(
+  comision: number,
+  porcentaje: number
+): number {
+  if (
+    !Number.isFinite(comision) ||
+    !Number.isFinite(porcentaje) ||
+    comision < 0 ||
+    porcentaje < 0 ||
+    porcentaje > 100
+  ) {
+    return 0;
+  }
 
-    const resultado = comision * (1 - porcentaje / 100);
-    return Number(resultado.toFixed(2));
+  const resultado = comision * (1 - porcentaje / 100);
+  return Number(resultado.toFixed(2));
 }
 export const calcularComision = (
   comisiones: Comision[],
@@ -32,53 +39,55 @@ export const calcularComision = (
   lenteDeContacto: number,
   metaProductosVip: MetaProductosVip | null,
   empresa: string,
-  porcentaje:number,
-  sucursal: string,
+  porcentaje: number,
+  sucursal: string
   //importe?:number
 ) => {
-  let llave:boolean = false
+  let llave: boolean = false;
   const productovip = gafaVip + monturaVip;
 
-
-  const llaveVip = metaProductosVip ? metaProductosVip.gafa + metaProductosVip.montura:0
-  console.log('llaveVip',llaveVip);
-  
+  const llaveVip = metaProductosVip
+    ? metaProductosVip.gafa + metaProductosVip.montura
+    : 0;
 
   let comisionProducto = 0;
 
-  /*if(sucursal?.includes("PARAGUAY")) {
-    comisionProducto = importe ? importe * 1 : 0;
-    return descontarPorcentajeAcomision(comisionProducto, porcentaje);
-  }*/
+ 
 
   if (Array.isArray(comisiones) && comisiones.length > 0) {
     const [mayorMonto, menorMonto] = comisiones.reduce(
       ([mayor, menor], actual) => [
         actual.monto > mayor.monto ? actual : mayor,
-        actual.monto < menor.monto ? actual : menor
+        actual.monto < menor.monto ? actual : menor,
       ],
-      [{monto: 0}, {monto: Infinity}]
+      [{ monto: 0 }, { monto: Infinity }]
     );
-        
-    if (empresa === "OPTICENTRO") {  
-      if (metaProductosVip && productovip >= llaveVip && lenteDeContacto >= metaProductosVip.lenteDeContacto) {
+
+    if (empresa === "OPTICENTRO") {
+      if (
+        metaProductosVip &&
+        productovip >= llaveVip &&
+        lenteDeContacto >= metaProductosVip.lenteDeContacto
+      ) {
         comisionProducto += mayorMonto.monto;
-        llave=true
-      } else if (sucursal === 'OPTICENTRO PARAGUAY'){
+        llave = true;
+      } else if (sucursal === "OPTICENTRO PARAGUAY") {
         comisionProducto += mayorMonto.monto;
-      }else{
-          comisionProducto += menorMonto.monto;
+      } else {
+        comisionProducto += menorMonto.monto;
       }
     } else {
-    
       comisionProducto += mayorMonto.monto;
     }
   }
 
-  return  {comison: descontarPorcentajeAcomision(comisionProducto, porcentaje), llave}
+  return {
+    comison: descontarPorcentajeAcomision(comisionProducto, porcentaje),
+    llave,
+  };
 };
 
-export function calcularComisionTotal (
+export function calcularComisionTotal(
   ventas: VentaElement[],
   metaProductosVip: MetaProductosVip | null,
   gafaVip: number,
@@ -86,33 +95,74 @@ export function calcularComisionTotal (
   lenteDeContacto: number,
   empresa: string,
   sucursal: string
- ) {
+) {
   return ventas.reduce((acc, venta) => {
-    const importeTotal = venta.detalle.reduce((acc, detalle) => acc + detalle.importe, 0);
+    const importeTotal = venta.detalle.reduce(
+      (acc, detalle) => acc + detalle.importe,
+      0
+    );
     const porcentajeDescuento = porcentaje(importeTotal, venta.descuento);
-    return acc + venta.detalle.reduce((acc, detalle) => {
-      const comision = calcularComision(
-        detalle.comisiones,
-        gafaVip, 
-        monturaVip,
-        lenteDeContacto, 
-        metaProductosVip, 
-        empresa, 
-        porcentajeDescuento,
-        sucursal,
-       // detalle.importe  // Pasamos el importe del detalle actual
-      );  
-      return acc + comision.comison;
-    }, 0);
+    return (
+      acc +
+      venta.detalle.reduce((acc, detalle) => {
+        const comision = calcularComision(
+          detalle.comisiones,
+          gafaVip,
+          monturaVip,
+          lenteDeContacto,
+          metaProductosVip,
+          empresa,
+          porcentajeDescuento,
+          sucursal
+          // detalle.importe  // Pasamos el importe del detalle actual
+        );
+        return acc + comision.comison;
+      }, 0)
+    );
   }, 0);
 }
 
-export const totalImporte = (ventas: VentaElement[]) => {
-    let importe: number = 0;
-    for (const venta of ventas) {
-      for (const detalle of venta.detalle) {
-        importe += detalle.importe;
-      }
+export function extraerLlave(
+  ventas: VentaElement[],
+  metaProductosVip: MetaProductosVip | null,
+  gafaVip: number,
+  monturaVip: number,
+  lenteDeContacto: number,
+  empresa: string,
+  sucursal: string
+) {
+  let llave: boolean = false;
+
+  for (const venta of ventas) {
+    const importeTotal = venta.detalle.reduce(
+      (acc, detalle) => acc + detalle.importe,
+      0
+    );
+    const porcentajeDescuento = porcentaje(importeTotal, venta.descuento);
+    for (const detalle of venta.detalle) {
+      const comison = calcularComision(
+        detalle.comisiones,
+        gafaVip,
+        monturaVip,
+        lenteDeContacto,
+        metaProductosVip,
+        empresa,
+        porcentajeDescuento,
+        sucursal
+      );
+      llave = comison.llave;
     }
-    return importe;
+  }
+
+  return llave;
+}
+
+export const totalImporte = (ventas: VentaElement[]) => {
+  let importe: number = 0;
+  for (const venta of ventas) {
+    for (const detalle of venta.detalle) {
+      importe += detalle.importe;
+    }
+  }
+  return importe;
 };
