360fe093f6817f65e8d1bc449513b420ecf36f53
Juan carlos chocllu <j.chocllu@opti-system.com>
2025-06-20 12:15:26 -0400

    cambios

--- src/app/service/api.ts
+++ src/app/service/api.ts
@@ -2,11 +2,11 @@ import axios, { AxiosError } from "axios";
 
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/`,
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials:true
+  //withCredentials:true
 });
 
 function getToken() {
